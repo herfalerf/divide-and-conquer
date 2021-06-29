3fc15689169c25ba595879eb8090c28c2ed0d1e6
herfalerf <jpendy@gmail.com>
2021-06-29 00:55:04 -0500

    countZeroes done

---
+++ count-zeroes.js
@@ -0,0 +1,24 @@
+function countZeroes(arr) {
+    let leftIdx = 0;
+    let rightIdx = arr.length -1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) /2);
+        let middleVal = arr[middleIdx];
+        
+        
+    
+        if ((middleIdx === 0 || arr[middleIdx -1] === 1) && arr[middleIdx] === 0) {
+            return arr.length - middleIdx;
+        } else if ( arr[middleIdx] === 1) {
+            leftIdx = middleIdx + 1
+        } else {
+            rightIdx = middleIdx - 1
+        }
+        
+    }
+    return 0;
+  
+}
+
+module.exports = countZeroes
