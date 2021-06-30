3d5aa4fca9f6b3064d712029e28a556e4618d675
herfalerf <jpendy@gmail.com>
2021-06-30 01:14:20 -0500

    last sorted-frequency function was ot actually O(logn), this one might be?

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,25 +1,3 @@
-// function sortedFrequency(arr, val) {
-//   let leftIdx = 0;
-//   let rightIdx = arr.length - 1;
-
-//   while (leftIdx <= rightIdx) {
-//     let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-//     let middleVal = arr[middleIdx];
-//     let count = 0;
-
-//     if (arr[leftIdx] === val && arr[rightIdx] === val) {
-//       count = rightIdx - leftIdx + 1;
-//     } else if (middleVal < val) {
-//       leftIdx = middleIdx + 1;
-//     } else if (middleVal > val) {
-//       rightIdx = middleIdx - 1;
-//     } else {
-
-//     }
-//   }
-//   return -1;
-// }
-
 function sortedFrequency(arr, val) {
   let firstIdx = findFirst(arr, val);
 
