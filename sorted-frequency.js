defb351c5e9d8a270918049b8dbf73c553a91c47
herfalerf <jpendy@gmail.com>
2021-06-30 01:09:47 -0500

    last sorted-frequency function was ot actually O(logn), this one might be?

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,19 +1,81 @@
-function sortedFrequency(arr, val) {
-  let leftIdx = 0;
-  let rightIdx = arr.length - 1;
+// function sortedFrequency(arr, val) {
+//   let leftIdx = 0;
+//   let rightIdx = arr.length - 1;
 
+//   while (leftIdx <= rightIdx) {
+//     let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+//     let middleVal = arr[middleIdx];
+//     let count = 0;
+
+//     if (arr[leftIdx] === val && arr[rightIdx] === val) {
+//       count = rightIdx - leftIdx + 1;
+//     } else if (middleVal < val) {
+//       leftIdx = middleIdx + 1;
+//     } else if (middleVal > val) {
+//       rightIdx = middleIdx - 1;
+//     } else {
+
+//     }
+//   }
+//   return -1;
+// }
+
+function sortedFrequency(arr, val) {
+  let firstIdx = findFirst(arr, val);
+
+  if (firstIdx == -1) return firstIdx;
+  let lastIdx = findLast(arr, val);
+
+  return lastIdx - firstIdx + 1;
+}
+
+function findFirst(arr, val) {
+  leftIdx = 0;
+  rightIdx = arr.length - 1;
   while (leftIdx <= rightIdx) {
-    if (arr[leftIdx] === val && arr[rightIdx] === val) {
-      return rightIdx - leftIdx + 1;
-    }
-    if (arr[leftIdx] < val) {
-      leftIdx += 1;
-    }
-    if (arr[rightIdx] > val) {
-      rightIdx -= 1;
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+
+    if (arr[leftIdx] === val) {
+      return leftIdx;
+    } else if (middleVal < val) {
+      leftIdx = middleIdx + 1;
+    } else if (middleVal > val) {
+      rightIdx = middleIdx - 1;
+    } else {
+      rightIdx = middleIdx - 1;
+      middleIdx = Math.floor((leftIdx + rightIdx) / 2);
     }
   }
-  return -1;
+  if (arr[leftIdx] === val) {
+    return leftIdx;
+  } else {
+    return -1;
+  }
+}
+function findLast(arr, val) {
+  leftIdx = 0;
+  rightIdx = arr.length - 1;
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+
+    if (arr[rightIdx] === val) {
+      return rightIdx;
+    } else if (middleVal < val) {
+      leftIdx = middleIdx + 1;
+    } else if (middleVal > val) {
+      rightIdx = middleIdx - 1;
+    } else {
+      leftIdx = middleIdx + 1;
+      middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    }
+  }
+  if (arr[rightIdx] === val) {
+    return rightIdx;
+  } else {
+    return -1;
+  }
 }
 
 module.exports = sortedFrequency;
