d3cf85ac9a2c4514bef7a492e732c8dd3b5eea3b
herfalerf <jpendy@gmail.com>
2021-06-29 23:39:18 -0500

    sorted-frequency finished, all tests passsing

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,20 @@
-function sortedFrequency() {
+function sortedFrequency(arr, val) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
 
+  while (leftIdx <= rightIdx) {
+    if (arr[leftIdx] === val && arr[rightIdx] === val) {
+      return rightIdx - leftIdx + 1;
+    }
+    if (arr[leftIdx] < val) {
+      leftIdx += 1;
+    }
+    if (arr[rightIdx] > val) {
+      rightIdx -= 1;
+    }
+  }
+  return -1;
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
+6;
